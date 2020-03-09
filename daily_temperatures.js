d3dc3f784e8559c3783dfae536ac57a81ce58d2a
Yacinesaf <yacinesaf96@gmail.com>
2020-03-09 16:25:22 -0400

    daily temperatures stuck

--- daily_temperatures.js
+++ daily_temperatures.js
@@ -3,7 +3,14 @@ let input = [73, 74, 75, 71, 69, 72, 76, 73];
 function daysToWaitForWarm(arr) {
   let daysToWait = [];
   let j = 1;
-  for(let i =0; i < arr.length; i++) {
-    if
+  for(let i =0; i < arr.length-1; i++) {
+    if(arr[j] > arr[i]) {
+      daysToWait.push(j - i);
+      j +=1;
+    } else if (arr[j] < arr[i]) {
+      
+    }
   }
+  console.log(daysToWait)
 }
+daysToWaitForWarm(input)
