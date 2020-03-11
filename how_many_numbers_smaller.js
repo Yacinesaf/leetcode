9079ef2dcd842eba4a58310f9d7af65e61fdfe02
Yacinesaf <yacinesaf96@gmail.com>
2020-03-11 17:13:19 -0400

    stuck in another problem

---
+++ how_many_numbers_smaller.js
@@ -0,0 +1,26 @@
+let is = [8,1,2,2,3];
+
+function isSmall(arr) {
+  let obj = {};
+  for(let i= 0; i< arr.length; i++) {
+    if(2 in obj) {
+      obj['two'] = 0;
+    }
+    obj[arr[i]] = 0
+  }
+  let current = 0
+  while (current < is.length) {
+    for(let j = 1; j <= arr.length-1; j++) {
+      if(arr[current] === 2 && arr[current+1] === 2) {
+        current++
+      }
+      if(arr[current] > arr[j]) {
+        obj[arr[current]] +=1
+      }
+    }
+    current++
+  }
+  console.log(obj)
+}
+
+isSmall(is)
